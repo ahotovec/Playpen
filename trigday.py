88cd1d0a704b526bdfcadeb8a6595b27f42763d6
Alicia Hotovec-Ellis <ahotovec@gmail.com>
2014-04-16 11:08:27 -0700

    added some better print messages

--- trigday.py
+++ trigday.py
@@ -6,27 +6,28 @@ Playing around with ObsPy
 
 from obspy.fdsn import Client
 from obspy import UTCDateTime
-import obspy.signal
 from obspy.signal.trigger import classicSTALTA, triggerOnset
-from obspy.signal.trigger import plotTrigger
-from obspy.signal.cross_correlation import xcorr
 
 # Grab a day from IRIS (HSR is at Mount St. Helens)
 client = Client("IRIS")
 # This is a time period where I know lots of repeaters are happening
 t = UTCDateTime("2004-11-24T00:00:00.000")
+
+print('Grabbing waveforms...')
 st = client.get_waveforms("UW", "HSR", "--", "EHZ", t, t + 86400)
+print('Done!')
 
 # Detrend and merge, fill gaps with zeros, bandpass
 st = st.detrend()
 st = st.merge(method=1, fill_value=0)
 st = st.filter('bandpass', freqmin=1.0, freqmax=10.0, corners=2, zerophase=True)
 
-print("Close the window to continue...")
+# print("Close the window to continue...")
 # Helicorder plot
-st.plot(type='dayplot')
+# st.plot(type='dayplot')
 
 # STA/LTA ttrigger
+print('Triggering')
 tr = st[0]
 cft = classicSTALTA(tr.data, 80, 700) # 0.8 s short, 7 s long
 # NOTE HERE: first trigger will be no earlier than 699 samples in!
@@ -42,21 +43,24 @@ on_off = triggerOnset(cft, 3, 2)
 # Slice out the traces, we want to correlate them next!
 
 # First trigger
+print('Cutting out triggers')
 trigtimes = [on_off[0,0]]
-trigs = st.slice(t - 10 + 0.01*trigtimes[0], t + 20 +  0.01*trigtimes[0])
+trigs = st.slice(t - 10 + 0.01*trigtimes[0], t + 20 + 0.01*trigtimes[0])
 
 for n in range(1,len(on_off)):
     if on_off[n,0] > trigtimes[-1] + 1000:
         trigtimes.append(on_off[n,0])
         trigs = trigs.append(st[0].slice(t - 10 + 0.01*trigtimes[-1], t + 20 + 0.01*trigtimes[-1]))
 
-print(len(trigs))
+print('Number of triggers: ' + repr(len(trigs)))
 
 # Check on some of the triggers to see if they're okay
 # trigs.plot(type='dayplot', vertical_scaling_range=500)
-print("Check to see if the triggers look okay. Close each window to continue...")
-for n in range(0,25):
-    trigs[n].plot()
+# print("Check to see if the triggers look okay. Close each window to continue...")
+# for n in range(0,25):
+#     trigs[n].plot()
+
+
 
 
 # PCA of triggers
@@ -68,7 +72,5 @@ for n in range(0,25):
 # X - np.random.random((270,250))
 # pca = PCA()
 # pca.fit(X)
-# 
-
-
+#
 
